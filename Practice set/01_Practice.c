6543dcd1f32e2fd80a6d851a2553985b9c5387e9
ArGiT69 <arjitchamlagain777@gmail.com>
2026-05-29 00:22:51 +0545

    Add rectangle area calculation program

---
+++ Practice set/01_Practice.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int length;
+int breadth;
+int area;
+printf("enter the length of the rectangle");
+scanf("%d",&length);
+printf("enter the breadth of the rectangle");
+scanf("%d",&breadth);
+area=length*breadth;
+printf("the area of rectangle is %d",area);
+    return 0;
+}
