f25bce0613c5412a90365c34c91f2085bb58cf78
ArGiT69 <arjitchamlagain777@gmail.com>
2026-05-28 22:24:12 +0545

    Add first C program and create variables.c file

--- first.c
+++ first.c
@@ -1,5 +1,14 @@
+/*
+this is my first c program
+name= Arjit 
+class= 11th
+*/
 #include<stdio.h>
 int main(){
-    printf("My name is arjit");
+    int a;
+    a=7;
+  // this is a single line comment 
+  // yeh line mere dil ke boht karib hai bhai pls isko mat hatanaa
+    printf("the value of a is %d",a);
     return 0;
 }
