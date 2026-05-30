1b3d52cae7e0536cd537ce264d6697519e45dfb4
ArGiT69 <arjitchamlagain777@gmail.com>
2026-05-31 00:24:58 +0545

    Fix typo in tax calculator program comments for clarity

--- Practice set/Tax_Calculator.c
+++ Practice set/Tax_Calculator.c
@@ -1,3 +1,14 @@
+/*this proogram answers the question below:
+Calculate income tax paid by an employee to the government
+as per the slabs mentioned below:
+Income Slab
+Tax:
+2.5-5.0L:5%
+5.0L - 10.0L:20%
+30%:Above 10.0L
+Note that there is no tax below 2.5L. Take income amount
+as an input from the user.
+*/
 #include <stdio.h>
 int main() {
     int income;
