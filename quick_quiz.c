e040ab7bdd1a827d4a6e0303b02719af5a13b5b7
ArGiT69 <arjitchamlagain777@gmail.com>
2026-06-28 15:18:06 +0545

    Add program to print numbers from 10 to 20 using a while loop

---
+++ quick_quiz.c
@@ -0,0 +1,11 @@
+/* Quick Quiz: Write a C program to print the numbers from 10 to 20 using a while loop.
+*/
+#include <stdio.h>
+int main() {
+    int n = 10;
+    while (n <=20){
+   printf("the value of n is %d\n", n);
+   n= n+1;
+    }
+    return 0;
+}
