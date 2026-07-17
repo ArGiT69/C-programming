92df1aa35d7bf7391277bc64cd1cdd3b7a52f490
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-17 11:37:11 +0545

    Refactor prime number check to use a while loop

--- chapter_04_practice_set.c/08_problem.c
+++ chapter_04_practice_set.c/08_problem.c
@@ -1,17 +1,21 @@
+// this is problem 07 written while using while loop
 #include <stdio.h>
 int main() {
     int num;
     int prime = 0;
     printf("Enter a number:\n");
     scanf("%d", &num);
+
     if (num <= 1) {
         prime = 1;   // treat as "not prime"
     } else {
-        for (int i = 2; i <= num / 2; i++) {
+        int i = 2;
+        while (i <= num / 2) {
             if (num % i == 0) {
                 prime = 1;
                 break;
             }
+            i++;
         }
     }
 
