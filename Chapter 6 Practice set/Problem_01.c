df6eaf582d3824cd40fdede90f772a3f0bb9a3e6
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-19 12:17:04 +0545

    Add program to print variable address and value using pointers

---
+++ Chapter 6 Practice set/Problem_01.c
@@ -0,0 +1,17 @@
+/*
+ write a program to print the address of a variable. Use this address to get the 
+ value of the variable
+*/
+#include <stdio.h>
+ 
+int main(){ 
+ int i = 2;
+ int* ptr= &i;
+
+ printf("the address of i is %u\n", &i);
+ printf("the value of i is %d", *ptr);
+    return 0;
+}
+
+
+
