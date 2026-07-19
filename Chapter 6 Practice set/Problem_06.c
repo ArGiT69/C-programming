dbf48196bbf5453745af7f562397e8b413621c8c
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-19 14:28:38 +0545

    Add program to demonstrate pointer to pointer usage in C

---
+++ Chapter 6 Practice set/Problem_06.c
@@ -0,0 +1,20 @@
+/*
+Write a program to print the value of a variable i by using 
+"pointer to pointer" type of variable.
+*/
+#include<stdio.h>
+int main(){
+ int i = 2;
+ int* ptr1 = &i;
+ int** ptr2 = &ptr1;
+ printf("the value of i is %d\n", i);
+ printf("the value of i is %d\n", *ptr1);
+ printf("the value of i is %d\n", **ptr2);
+
+    return 0;
+}
+
+
+
+
+
