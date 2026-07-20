37bd13af69ed7d13b0883fe4f7c09297f413008e
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-20 21:38:23 +0545

    Add program to demonstrate input and output of arrays in C

---
+++ Chapter_07/02_arrays_input.c
@@ -0,0 +1,21 @@
+/* Arrays - Input */
+#include<stdio.h>
+ int main(){
+ int marks[5];
+ printf("enter marks of 5 students\n");
+ scanf("%d",&marks[0]);
+ scanf("%d",&marks[1]);
+ scanf("%d",&marks[2]);
+ scanf("%d",&marks[3]);
+ scanf("%d",&marks[4]);
+
+ printf("the marks of the students are:\n %d\n %d\n %d\n %d\n %d\n", marks[0],marks[1],marks[2],marks[3]
+ ,marks[4]);
+
+    return 0;
+ }
+
+
+
+
+
