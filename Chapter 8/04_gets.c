31aa0d1ed1ef74487db685309256c7400b215402
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-24 14:59:10 +0545

    Add program to demonstrate string input and output using gets and puts in C

---
+++ Chapter 8/04_gets.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+ char st[30];
+ gets(st); // the entered string is stored in st!
+
+ //  printf("%s", st);  
+ puts(st);//Prints the string and places the cursor on the next line
+printf("hey");
+    return 0;
+}
