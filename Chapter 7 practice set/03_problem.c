bf345fd99527da41fa888f70460470c976ec0548
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-24 13:37:51 +0545

    Add program to create an array of 10 integers for the multiplication table of 5 and print using pointer arithmetic in C

---
+++ Chapter 7 practice set/03_problem.c
@@ -0,0 +1,20 @@
+/*
+ write a program to create an array
+ of 10 integers and store multiplication table of 5 in it.
+  Then print the array using pointer arithmetic.
+*/
+#include <stdio.h>
+int main(){
+ int arr[10];
+ for (int i=0; i<10; i++){
+  arr[i] = 5 * (i+1);
+ }
+ for (int i=0; i<10; i++){
+ printf("the value of 5 X %d = %d\n", i+1, arr[i]);
+ }
+    return 0;
+}
+
+
+
+
