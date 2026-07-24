55649d1b814c01412448d83a5f645304a5548c96
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-24 20:47:00 +0545

    Refactor employee input/output formatting in the employee details program

--- Chapter 9/02_Quick_Quiz.c
+++ Chapter 9/02_Quick_Quiz.c
@@ -15,39 +15,30 @@ int main(){
     struct employee e1, e2, e3;
     printf("enter the employee code\n");
     scanf("%d", &e1.code);
-
-
     printf("enter the salary\n");
     scanf("%f", &e1.salary);
-
-
     printf("enter the name\n");
     scanf("%s", e1.name);
+
     printf("%d %f %s\n",e1.code,e1.salary,e1.name);
 
 
       printf("enter the employee code\n");
     scanf("%d", &e2.code);
-
-
     printf("enter the salary\n");
     scanf("%f", &e2.salary);
-
-
     printf("enter the name\n");
     scanf("%s", e2.name);
+
     printf("%d %f %s\n",e2.code,e2.salary,e2.name);
 
       printf("enter the employee code\n");
     scanf("%d", &e3.code);
-
-
     printf("enter the salary\n");
     scanf("%f", &e3.salary);
-
-
     printf("enter the name\n");
     scanf("%s", e3.name);
+    
     printf("%d %f %s\n",e3.code,e3.salary,e3.name);
 
 
