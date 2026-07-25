f0b147f14d0d23706e108cbc6fb0c47ffafdc787
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-25 16:08:03 +0545

    Refactor Date structure definition for improved readability

--- Chapter 9 practice set/07_problem.c
+++ Chapter 9 practice set/07_problem.c
@@ -3,14 +3,15 @@ Q. Write a structure capable of storing dates. Write a
 function to compare those dates.
 */
 #include<stdio.h>
+
+
 struct Date{
 int dd;
 int mm;
 int yyyy;
-
-
-
 };
+
+
 int compare(struct Date d1, struct Date d2){
     //if d1 is in the future, return 1 
     if( (d1.yyyy==d2.yyyy) && (d1.mm == d2.mm) && (d1.dd == d2.dd)){
@@ -39,16 +40,10 @@ else if (d1.dd < d2.dd)
 {
    return -1; 
 }
-
-{
-
 }
 
-
-}
 
 int main(){
-
     struct Date d1 = {07, 10, 2010};
     struct Date d2 = {02, 9, 2010};
     printf("%d",compare(d1,d2));
