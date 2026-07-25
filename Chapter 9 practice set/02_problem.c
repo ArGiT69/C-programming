dc1040e080a7e9cc57a1a30c09a36751cd545dc2
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-25 12:39:38 +0545

    Add program to implement a function that sums two two-dimensional vectors in C

---
+++ Chapter 9 practice set/02_problem.c
@@ -0,0 +1,25 @@
+/*
+Q. Write a function 'sumVector' which returns the sum of two vectors passed to it.
+the vectors must be two dimensional.
+*/
+#include <stdio.h>
+struct vector{
+    int i;
+    int j;
+};
+struct vector sumVector(struct vector v1, struct vector v2){
+struct vector v3 = {v1.i+v2.i, v1.j+v2.j};
+return v3;
+};
+
+int main(){
+    struct vector v1 = {1,2};
+    struct vector v2 = {5,6};
+    struct vector v3 = sumVector(v1,v2);
+    printf("the sum of v1 and v2 is v3 = %di+%dj",v3.i, v3.j);
+
+    return 0;
+}
+
+
+
