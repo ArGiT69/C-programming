27f3a78c7d9ddb0aad2daf0ba24455199aedd488
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-25 16:10:45 +0545

    Add program to implement a structure for representing dates and a comparison function in C (same as problem 7 but with typedef keyword)

---
+++ Chapter 9 practice set/08_problem.c
@@ -0,0 +1,54 @@
+/*
+Q. Solve the problem 7 in typedef keyword
+*/
+#include<stdio.h>
+
+
+typedef struct Date{
+int dd;
+int mm;
+int yyyy;
+}D;
+
+
+int compare(D d1, D d2){
+    //if d1 is in the future, return 1 
+    if( (d1.yyyy==d2.yyyy) && (d1.mm == d2.mm) && (d1.dd == d2.dd)){
+
+        return 0; 
+    }
+if(d1.yyyy>d2.yyyy){
+    return 1;
+}
+if(d1.yyyy<d2.yyyy){
+    return -1;
+}
+else if (d1.mm > d2.mm)
+{
+   return 1; 
+}
+else if (d1.mm < d2.mm)
+{
+   return -1; 
+}
+else if (d1.dd > d2.dd)
+{
+   return 1; 
+}
+else if (d1.dd < d2.dd)
+{
+   return -1; 
+}
+}
+
+
+int main(){
+    D d1 = {07, 10, 2010};
+    D d2 = {02, 9, 2010};
+    printf("%d",compare(d1,d2));
+    return 0;
+}
+
+
+
+
