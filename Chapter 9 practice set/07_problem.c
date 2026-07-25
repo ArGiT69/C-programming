dc3fe4d308a2c26425ed2303242e4371e5b3a60a
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-25 16:07:20 +0545

    Add program to implement a structure for storing dates and a comparison function in C

---
+++ Chapter 9 practice set/07_problem.c
@@ -0,0 +1,59 @@
+/*
+Q. Write a structure capable of storing dates. Write a 
+function to compare those dates.
+*/
+#include<stdio.h>
+struct Date{
+int dd;
+int mm;
+int yyyy;
+
+
+
+};
+int compare(struct Date d1, struct Date d2){
+    //if d1 is in the future, return 1 
+    if( (d1.yyyy==d2.yyyy) && (d1.mm == d2.mm) && (d1.dd == d2.dd)){
+
+        return 0; 
+    }
+if(d1.yyyy>d2.yyyy){
+    return 1;
+}
+if(d1.yyyy<d2.yyyy){
+    return -1;
+}
+else if (d1.mm > d2.mm)
+{
+   return 1; 
+}
+else if (d1.mm < d2.mm)
+{
+   return -1; 
+}
+else if (d1.dd > d2.dd)
+{
+   return 1; 
+}
+else if (d1.dd < d2.dd)
+{
+   return -1; 
+}
+
+{
+
+}
+
+
+}
+
+int main(){
+
+    struct Date d1 = {07, 10, 2010};
+    struct Date d2 = {02, 9, 2010};
+    printf("%d",compare(d1,d2));
+    return 0;
+}
+
+
+
