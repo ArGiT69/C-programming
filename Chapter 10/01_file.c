47226c5181a7d2061c27beb349559893c35fce94
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-26 21:24:10 +0545

    Fix file closure placement in file reading program

--- Chapter 10/01_file.c
+++ Chapter 10/01_file.c
@@ -7,5 +7,6 @@ int main(){
     printf("The value of num is %d\n", num);
     fscanf(ptr,"%d",&num);
     printf("The value of num is %d\n", num);
+    fclose(ptr);//it closes the file whenever you want once the reading is completed
     return 0;
 }
