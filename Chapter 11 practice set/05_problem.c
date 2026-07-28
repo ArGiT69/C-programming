90dcb0ccf68826a89ae9cccbf657101a17351bb6
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-28 19:41:15 +0545

    Format code in 05_problem.c for better readability

--- Chapter 11 practice set/05_problem.c
+++ Chapter 11 practice set/05_problem.c
@@ -2,23 +2,23 @@
 Q. Create an array of multiplication table of 7 upto 10 ( 7 x 10 = 70).
 Use realloc to make it store 15 number (from 7 x 1 to 7 x 15).
 */
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
- int n = 10;
- int* ptr;
- ptr = (int*) malloc(n* sizeof(int));
- for(int i=0; i<n; i++){
-    printf("7 x %d = %d\n", i+1, 7*(i+1));
- }
- printf("now the n changes from 10 to 15\n");
- n = 15;
- ptr = (int*) realloc(ptr, 15* sizeof(int));
- for(int i=0; i<n; i++){
-    printf("7 x %d = %d\n", i+1, 7*(i+1));
- }
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 10;
+    int *ptr;
+    ptr = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("7 x %d = %d\n", i + 1, 7 * (i + 1));
+    }
+    printf("now the n changes from 10 to 15\n");
+    n = 15;
+    ptr = (int *)realloc(ptr, 15 * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("7 x %d = %d\n", i + 1, 7 * (i + 1));
+    }
     return 0;
 }
-
-
-
