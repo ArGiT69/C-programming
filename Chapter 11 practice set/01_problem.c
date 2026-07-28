01b3a23a93d1fd3944f9411de401f9d7ffe18cbc
ArGiT69 <arjitchamlagain777@gmail.com>
2026-07-28 19:00:45 +0545

    Add implementation of dynamic array creation example in 01_problem.c

---
+++ Chapter 11 practice set/01_problem.c
@@ -0,0 +1,15 @@
+/*
+Q. Write a program to dynamically create an arrray of size 6 capable of storing
+6 integers
+*/
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n = 6;
+    int *ptr;
+    ptr = (int*) malloc(n* sizeof(int));
+    ptr[0] = 24;
+    printf("%d\n", ptr[0]);
+    return 0;
+}
